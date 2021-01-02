5245689b31d2639702fee652e43d039cf49feb66
Ivanna <iva.63@hotmail.com>
2021-01-02 16:52:22 +0100

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,94 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-
     // Scaffold - to create basic layout
     home: Home(),
-
-    /*
-    Scaffold(
-      // Specifies how the AppBar will look at the top
-        appBar: AppBar(
-          title: Text(
-              "Flutter Example 1",
-              style: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold
-              ),
-          ),
-          backgroundColor: Colors.lightBlue[300],
-        ),
-      /*body: Center(
-        /*child:Text(
-            "Email",
-          style: TextStyle(
-            fontSize: 18.0
-          ),
-        )*/
-        child: Image(
-          image: AssetImage('assets/luulz.jpg'),
-        ),
-      )*/
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-
-          Image(
-            image: AssetImage('assets/luulz.jpg'),
-            width: 400.0,
-            height: 160.0,
-            //alignment: Alignment.center,
-          ),
-
-          Row(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {},
-                child: Text("BUTTON"),
-              ),
-
-              RaisedButton(
-                onPressed: () {},
-                child: Text("BUTTON"),
-              ),
-            ]
-          ),
-
-          Row(
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {},
-                  child: Text("BUTTON"),
-                ),
-
-                RaisedButton(
-                  onPressed: () {},
-                  child: Text("BUTTON"),
-                ),
-              ]
-          ),
-
-          Row(
-          children: <Widget> [
-            Text("Email"),
-          /*
-            TextField(
-              obscureText:  true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter e-mail here',
-              ),
-            ),*/
-          ]
-          )
-        ],
-
-      ),
-    ),*/
   ));
 }
-
 
 class Home extends StatelessWidget {
   @override
@@ -99,44 +15,26 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           "Flutter Example 1",
-          style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.lightBlue[300],
       ),
-      /*body: Center(
-        /*child:Text(
-            "Email",
-          style: TextStyle(
-            fontSize: 18.0
-          ),
-        )*/
-        child: Image(
-          image: AssetImage('assets/luulz.jpg'),
-        ),
-      )*/
 
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-
           SizedBox(
             height: 5.0,
           ),
-
           Image(
             image: AssetImage('assets/luulz.jpg'),
             width: 400.0,
             height: 160.0,
             //alignment: Alignment.center,
           ),
-
           SizedBox(
             height: 30.0,
           ),
-
           Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
@@ -144,18 +42,14 @@ class Home extends StatelessWidget {
                   onPressed: () {},
                   child: Text("BUTTON"),
                 ),
-
                 RaisedButton(
                   onPressed: () {},
                   child: Text("BUTTON"),
                 ),
-              ]
-          ),
-
+              ]),
           SizedBox(
             height: 20.0,
           ),
-
           Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
@@ -163,28 +57,24 @@ class Home extends StatelessWidget {
                   onPressed: () {},
                   child: Text("BUTTON"),
                 ),
-
                 RaisedButton(
                   onPressed: () {},
                   child: Text("BUTTON"),
                 ),
-              ]
-          ),
-
+              ]),
           SizedBox(
             height: 30.0,
           ),
-
           Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget> [
-                Text("Email",
+              children: <Widget>[
+                Text(
+                  "Email",
                   style: TextStyle(
                     fontSize: 14.0,
                     color: Colors.grey,
                   ),
                 ),
-
                 ConstrainedBox(
                   constraints: BoxConstraints(
                     minWidth: 180.0,
@@ -192,144 +82,11 @@ class Home extends StatelessWidget {
                     minHeight: 40.0,
                     maxHeight: 40.0,
                   ),
-                  child: TextField(
-
-                  ),
+                  child: TextField(),
                 ),
-
-                /*
-            TextField(
-              obscureText:  true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter e-mail here',
-              ),
-            ),*/
-              ]
-          )
+              ])
         ],
-
       ),
-
-
     );
   }
 }
-
-
-
-
-
-/*
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-        home: MyHomePage(title: "Flutter Home Demo Example"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
